1419268b0a8d5539f344762798a14cb257220f6d
Anorpi <5262122@gmail.com>
2016-08-15 07:45:43 +0000

    add some function

--- index.sh
+++ index.sh
@@ -1,17 +1,17 @@
 #/bin/bash
 
-##Get eth0's ipv4 address
+#Get eth0's ip address
 server_ip=`ifconfig eth0|grep 'inet addr'|awk '{print $2}'|cut -c6-`
 echo $server_ip
 
-##Get public ipv4 from public API
+#Get public ipv4 from public API
 server_ip=`curl  -s http://ipecho.net/plain`
 
-##Gennerate a password
+#Gennerate a password
 client_pass=`< /dev/urandom tr -dc A-Za-z0-9 | head -c${1:-12};echo;`
 echo $client_pass
 
-##Check a package installed or not
+#Check a package installed or not
 Pythonsetuptools_Status=`yum list python-setuptools|grep "Error"`
 if [ "$Pythonsetuptools_Status" != "" ] ;then
         echo "Installing python-setuptools"
@@ -20,7 +20,7 @@ else
         echo "Python-setuptools:ok"
 fi
 
-##Check a command existed or  not
+#Check a command existed or  not
 qrencode --version > /dev/null 2>&1
 if [ $? != 0 ]; then
         echo "Installing qrencode"
@@ -36,7 +36,7 @@ if [ "$user_name" != "root" ]; then
         exit
 fi
 
-## Check support Centos 6.x
+# Check support Centos 6.x
 Support_OS=`cat /etc/issue|grep -E 6\.[0-9]`
 if [ "$Support_OS" != "" ] ;then
         echo "System version:ok"
@@ -45,23 +45,23 @@ else
         exit
 fi
 
-##Get a timestamp
+#Get a timestamp
  ##methmod1
 time_stamp_1=`date +%Y%m%d%H%M%S`
  ##methmod2
 time_stamp_2=`date +%s`
 
-##Backup file with a timestamp
+#Backup file with a timestamp
 time_stamp=`date +%Y%m%d%H%M%S`
 if [ -f /etc/shadowsocks.json ] ; then
         echo "/etc/shadowsocks.json file exits,rename to shadowsocks.json.bak${time_stamp}"
         mv /etc/shadowsocks.json /etc/shadowsocks.json.bak${time_stamp}
 fi
 
-##Allow a tcp port,eg:allow 8388 port can access.
+#Allow a tcp port,eg:allow 8388 port can access.
 iptables -I INPUT -p tcp --dport 8388 -j ACCEPT
 
-##Check a command execute succeed
+#Check a command execute succeed
 service nginx status 
 if [ "$?" != "0" ]; then
         echo  "command execute failed."
@@ -93,3 +93,25 @@ client_base64_2=`echo "thisa${var}"|base64`
 #Output shadowsocks client QR image,need install qrencode:'yum -y install qrencode'
 client_base64=`echo "aes-256-cfb:$client_password@$server_ip:8388"|base64`
 echo "ss://$client_base64"| qrencode -o - -t UTF8
+
+#Delete .gz backup files
+file_type=gz
+file_num=4
+old_list=`ls -t $PWD/*.$file_type 3>/dev/null|tail -n +$file_num`
+if [ -z "$old_list" ];then
+        exit;
+else
+        rm -rf $old_list
+fi
+
+#an example,touch a new test.txt file with some line.
+cat <<EOF>test.txt
+#one line
+#two line
+servername 8.8.8.8
+EOF
+
+#another example,append line in text.txt file
+cat <<EOF>>test.txt
+#just append a new line
+EOF
